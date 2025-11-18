f4911d3ef026151a6f5c4f0e6f395095c880854c
shathipaul <shathipaul11@gmail.com>
2025-11-18 15:59:44 +0600

    fixed boooks and author

--- grand-duchy-ebook-frontend/src/components/home/about-author/AboutAuthor.tsx
+++ grand-duchy-ebook-frontend/src/components/home/about-author/AboutAuthor.tsx
@@ -3,19 +3,28 @@ import author from "@/assets/images/author.png";
 
 const AboutAuthor = () => {
   return (
-    <div className="grid md:grid-cols-2 items-center justify-center gap-4  py-8 lg:py-20">
-      <div className="flex flex-col gap-4 max-w-[600px]">
-        <h1 className="text-end uppercase italic text-4xl lg:text-6xl">
-          Mustafa <br /> Sabri Bülbül
-        </h1>
-        <p className="text-end italic lg:text-xl ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
-          impedit quaerat? Eligendi blanditiis dolor voluptatibus officiis eum
-          quas reiciendis rem.
-        </p>
-      </div>
-      <div className="max-w-[600px] grayscale">
-        <Image src={author} alt="" />
+    <div>
+      <h3 className="text-center text-beige text-[80px] md:text-[140px] lg:text-[200px]">
+        Author
+      </h3>
+
+      <div className="md:-mt-24 lg:-mt-32 grid md:grid-cols-2 items-center gap-8 xl:gap-0 justify-items-center">
+        <div className="md:hidden w-full h-full object-cover  grayscale">
+          <Image src={author} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="flex flex-col gap-4 lg:max-w-[600px] l">
+          <h1 className="md:text-end titleFont text-5xl font-semibold tracking-wide">
+            Mustafa <br /> Sabri Bülbül
+          </h1>
+          <p className="md:text-end italic lg:text-xl ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+            impedit quaerat? Eligendi blanditiis dolor voluptatibus officiis eum
+            quas reiciendis rem.
+          </p>
+        </div>
+        <div className="hidden md:block max-w-[600px] grayscale w-full h-full object-cover">
+          <Image src={author} alt="" className="w-full h-full object-cover" />
+        </div>
       </div>
     </div>
   );
--- grand-duchy-ebook-frontend/src/components/home/books/Books.tsx
+++ grand-duchy-ebook-frontend/src/components/home/books/Books.tsx
@@ -18,42 +18,48 @@ const Books = () => {
   const books = data?.data as IBooks[];
   console.log(books);
   return (
-    <div className="mb-8 max-w-[250px] mx-auto md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl overflow-hidden">
-      <Swiper
-        slidesPerView={1}
-        breakpoints={{
-          768: { slidesPerView: 4 },
-          1280: { slidesPerView: 7 },
-        }}
-        spaceBetween={40}
-        speed={1500}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        grabCursor={true}
-        mousewheel={{ forceToAxis: true }}
-        loop={true}
-        modules={[Autoplay, Mousewheel]}
-        className="overflow-hidden w-full"
-      >
-        {books.map((data, index) => (
-          <SwiperSlide key={index}>
-            <Link href={`/book/${data._id}`}>
-              <div className="w-full h-full  overflow-hidden">
-                <Image
-                  className="w-full h-full object-contain"
-                  src={data.image}
-                  alt={data.title}
-                  priority={true}
-                  width={600}
-                  height={600}
-                />
-              </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div>
+      <h3 className="text-center text-beige text-[80px] md:text-[140px] lg:text-[200px]">
+        Books
+      </h3>
+      <div className="md:-mt-20 lg:-mt-28 mb-20 max-w-[250px] mx-auto md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl overflow-hidden">
+        <Swiper
+          slidesPerView={1}
+          breakpoints={{
+            768: { slidesPerView: 3 },
+            1280: { slidesPerView: 5 },
+            1440: { slidesPerView: 7 },
+          }}
+          spaceBetween={40}
+          speed={1500}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          grabCursor={true}
+          mousewheel={{ forceToAxis: true }}
+          loop={true}
+          modules={[Autoplay, Mousewheel]}
+          className="overflow-hidden w-full"
+        >
+          {books.map((data, index) => (
+            <SwiperSlide key={index}>
+              <Link href={`/book/${data._id}`}>
+                <div className="w-full h-full  overflow-hidden">
+                  <Image
+                    className="w-full h-full object-contain"
+                    src={data.image}
+                    alt={data.title}
+                    priority={true}
+                    width={600}
+                    height={600}
+                  />
+                </div>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
